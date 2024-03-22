47644eba49b3aeea5f6f59e3ce92da9f2dbc9206
kevinaswind <yhjiang730@icloud.com>
2024-03-22 15:11:20 +0800

    Clean the game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,7 +17,7 @@ const GameGrid = ({ gameQuery }: Props) => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={10}
+        spacing={14}
         paddingY={5}
       >
         {isLoading &&
